4e030644156d10a62486823b87e506060a099fd1
Thomas Hänig <thomas.haenig@akquinet.de>
2017-09-01 10:37:03 +0200

    including webpack-dev-servrer and CheckerPlugin

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,20 +1,36 @@
 const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
 	entry: './main',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		
 	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx']
-	},
+	
 	module: {
 		loaders: [
 			{
 				test: /\.tsx?$/,
+				exclude: [
+					path.resolve(__dirname, 'node_modules')
+				],
 				loader: 'awesome-typescript-loader'
 			}
 		]
-	}
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx']
+	},
+	devtool: 'source-map',
+	devServer: {
+		contentBase: path.join(__dirname, "./"),
+		compress: true,
+		port: 8080
+	},
+	plugins: [
+		new CheckerPlugin()
+	]
+	
 };
