698adb1192f44904bc21df3fdf62c5200df28f49
Thomas Hänig <thomas.haenig@akquinet.de>
2017-09-11 09:31:32 +0200

    create css output file, in progress

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const { CheckerPlugin } = require('awesome-typescript-loader');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+// var webpackStream = require( 'webpack-stream' );
+
+
+// webpackStream(config, require('webpack'));
+
+
 
 module.exports = {
 	entry: [
 		'./main',
-		"./style.less"
+		'./style'
 	],
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist')
 	},
 
@@ -34,20 +43,25 @@ module.exports = {
 					path.resolve(__dirname, 'node_modules')
 				],
 				loader: 'awesome-typescript-loader'
-            },
+			},
 			{
                 test: /\.less$/,
-				exclude: [
-					path.resolve(__dirname, 'node_modules')
-				],
-				use: [{
-					loader: "style-loader"
-				},{
-					loader: "css-loader"
-				}, {
-					loader: "less-loader"
-				}]
-            }, {
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+			// {
+            //     test: /\.less$/,
+			// 	exclude: [
+			// 		path.resolve(__dirname, 'node_modules')
+			// 	],
+			// 	use: [{
+			// 		loader: "style-loader"
+			// 	},{
+			// 		loader: "css-loader"
+			// 	}, {
+			// 		loader: "less-loader"
+			// 	}]
+			// }
+			, {
 				test: /\.(jpg|png|svg)$/,
 				loader: 'url-loader',
 				options: {
@@ -57,15 +71,16 @@ module.exports = {
         ]
 	},
 	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx', 'less']
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.less']
 	},
-	devtool: 'source-map',
+	
 	devServer: {
 		contentBase: path.join(__dirname, "./"),
 		compress: true,
 		port: 8080
 	},
 	plugins: [
-		new CheckerPlugin()
+		new CheckerPlugin(),
+		new ExtractTextPlugin("[name].css")
 	]	
 };
